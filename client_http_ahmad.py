50ab9c73ae8a39443b6092c651477d14a316d82e
Muhammad Fayyaz <mfayyaz.bese17seecs@seecs.edu.pk>
2019-05-11 18:40:08 +0500

    implemented the write function
    for those servers which do not support  byte ranges

--- client_http_ahmad.py
+++ client_http_ahmad.py
@@ -5,7 +5,11 @@ def write_file(msg,x,ftype):
         f = open('Socket'+str(x)+'.' +ftype, 'ab')
         f.write(msg)
         f.close()
-
+def write_file(msg,x,ftype,new):
+        f = open('Socket'+str(x)+'.' +ftype, 'wb')
+        f.write(msg)
+        f.close()
+        
 def get_server_addess(site):
 #procssing to string to sepearate server and address for http request
         if site.startswith('http://'):
@@ -44,54 +48,55 @@ def download_file(site):
         contentlength = int(s[b'Content-Length'])
         type1 = s[b'Content-Type'].split(b'/')[1]
         type1 = str(type1,'utf-8')
-
-        if b'bytes' in s[b'Accept-Ranges']  :
-                #loop for multiple get requests
-                q=10 #Incase of multiple connection we get this variable from user
-                previousB= 0
-                nextB = contentlength//q
-                #nextB = 18000               
-                full_msg = b''
-                c=0
-                for x in range (1,11):
-                        
-                        bRange = "%s-%s"%(previousB,nextB)
-                        request = 'GET ' + address + ' HTTP/1.1\r\nHOST: ' + server + '\r\nRange:bytes=' + bRange + '\r\n\r\n'
-                        print(request)
-                        request_header = bytes(request,'utf-8')  
-                        cs.send(request_header)
-                        previousB = nextB + 1
-                        flag = nextB
-                        if nextB>contentlength:
-                                flag = contentlength
-                        nextB += contentlength//q
-                        print(nextB)
-                        new_msg= True
-                        #c=True
-                        
-                        while c<flag:
-                            msg = cs.recv(15000)
-                            
-                            if new_msg:
+        if b'Accept-Ranges' in s:
+                if b'bytes' in s[b'Accept-Ranges']  :
+                        #loop for multiple get requests
+                        q=10 #Incase of multiple connection we get this variable from user
+                        previousB= 0
+                        nextB = contentlength//q
+                        #nextB = 18000               
+                        full_msg = b''
+                        c=0
+                        for x in range (1,11):
                                 
-                                head = msg.split(b'\r\n\r\n')
-                                header=(len(head[0]))+4
-                                print(head[0])
-                                print(header)
-                                msg = head[1]
-                                new_msg = False        
-                            #print(len(msg))
-                            c+=len(msg)
-                            full_msg = full_msg + msg
-                            print(len(full_msg))
-                            if len(full_msg)== contentlength:
-                                print(full_msg[header:])
-                                print('Done through special loop')
-                                write_file(full_msg,3,type1)
-                                new_msg = True
-                                full_msg = b''
-                                c= False
-                cs.close()
+                                bRange = "%s-%s"%(previousB,nextB)
+                                request = 'GET ' + address + ' HTTP/1.1\r\nHOST: ' + server + '\r\nRange:bytes=' + bRange + '\r\n\r\n'
+                                print(request)
+                                request_header = bytes(request,'utf-8')  
+                                cs.send(request_header)
+                                previousB = nextB + 1
+                                flag = nextB
+                                if nextB>contentlength:
+                                        flag = contentlength
+                                nextB += contentlength//q
+                                print(nextB)
+                                new_msg= True
+                                #c=True
+                                
+                                while c<flag:
+                                    msg = cs.recv(15000)
+                                    
+                                    if new_msg:
+                                        
+                                        head = msg.split(b'\r\n\r\n')
+                                        header=(len(head[0]))+4
+                                        print(head[0])
+                                        print(header)
+                                        msg = head[1]
+                                        new_msg = False        
+                                    #print(len(msg))
+                                    c+=len(msg)
+                                    full_msg = full_msg + msg
+                                    print(len(full_msg))
+                                    write_file(msg,3,type1)
+                                    if len(full_msg)== contentlength:
+                                        print(full_msg[header:])
+                                        print('Done through special loop')
+                                        #write_file(full_msg,3,type1)
+                                        new_msg = True
+                                        full_msg = b''
+                                        c= False
+                        cs.close()
                 
         else:
                 print('Simulataneous connection not allowed using single connection')
@@ -112,14 +117,14 @@ def download_file(site):
                         print(header)
                         msg = head[1]
                         new_msg = False
-                    print("tis one did it" )
+                    #print("tis one did it" )
                     full_msg += msg
-
+                    write_file(msg,2,type1,2)
                     if len(full_msg)== contentlength:
                                 print(full_msg[header:])
                                 print('Done')
                                 new_msg = True
-                                write_file(full_msg,2)
+#                                write_file(full_msg,2)
                                 full_msg = ""
                                 c= False
                                 cs.close()         
@@ -127,12 +132,12 @@ def download_file(site):
 
                 
 #Main Function
-#site = 'http://open-up.eu/files/Berlin%20group%20photo.jpg?width=600&height=600'
+site = 'http://open-up.eu/files/Berlin%20group%20photo.jpg?width=600&height=600'
 #site = 'http://people.unica.it/vincenzofiorentini/files/2012/04/Halliday-Fundamentals-of-Physics-Extended-9th-HQ.pdf'
 #site = 'http://africhthy.org/sites/africhthy.org/files/styles/slideshow_large/public/Lukuga.jpg?itok=M6ByJTZQ'
 #site = 'http://ipaeg.org/sites/ipaeg.org/files/styles/medium/public/IMG_0499.JPG?itok=U8KP8f4j'
 #site = 'http://s0.cyberciti.org/images/misc/static/2012/11/ifdata-welcome-0.png'
-site = 'http://i.imgur.com/z4d4kWk.jpg'
+#site = 'http://i.imgur.com/z4d4kWk.jpg'
 
 #server,address = get_server_address(site)
 download_file(site)
